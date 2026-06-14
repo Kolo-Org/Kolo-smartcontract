1c440e0c476ba4e7a95395aa9857c18c8494b8df
queenode <queenode@users.noreply.github.com>
2026-06-14 20:27:05 +0100

    test: add tests for add_member and contribute

--- contracts/savings_group/src/test.rs
+++ contracts/savings_group/src/test.rs
@@ -2,6 +2,7 @@
 
 use super::*;
 use soroban_sdk::{testutils::Address as _, Address, Env, String};
+use soroban_sdk::token;
 
 #[test]
 fn test_initialize() {
@@ -15,8 +16,6 @@ fn test_initialize() {
     let contribution_amount = 1000;
 
     client.initialize(&admin, &token, &name, &contribution_amount);
-
-    // Further validation could be added by exposing getters for these values
 }
 
 #[test]
@@ -33,4 +32,42 @@ fn test_double_initialize() {
 
     client.initialize(&admin, &token, &name, &contribution_amount);
     client.initialize(&admin, &token, &name, &contribution_amount);
+}
+
+#[test]
+fn test_add_member() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, KoloSavingsContract);
+    let client = KoloSavingsContractClient::new(&env, &contract_id);
+
+    let admin = Address::generate(&env);
+    let token = Address::generate(&env);
+    let name = String::from_str(&env, "Test Group");
+    let contribution_amount = 1000;
+
+    client.initialize(&admin, &token, &name, &contribution_amount);
+
+    env.mock_all_auths();
+    let member1 = Address::generate(&env);
+    client.add_member(&member1);
+}
+
+#[test]
+#[should_panic(expected = "Not a member")]
+fn test_contribute_not_member() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, KoloSavingsContract);
+    let client = KoloSavingsContractClient::new(&env, &contract_id);
+
+    let admin = Address::generate(&env);
+    let token_admin = Address::generate(&env);
+    let token = env.register_stellar_asset_contract(token_admin.clone());
+    let name = String::from_str(&env, "Test Group");
+    let contribution_amount = 1000;
+
+    client.initialize(&admin, &token, &name, &contribution_amount);
+
+    env.mock_all_auths();
+    let not_member = Address::generate(&env);
+    client.contribute(&not_member, &1000);
 }
